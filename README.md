7d1e5e67f8cc9394720d90c8ef30f71200b5a1a9
Fredrik Wollsén <fredrik@neam.se>
2012-09-22 22:43:55 -0700

    Basic project files

---
+++ README.md
@@ -0,0 +1,9 @@
+# dbstep
+
+CSV-Data Overtone Dubstep Generator
+
+Turns csv data (originated from NASDAQ quote data) into Dubstep music using Overtone
+
+## License
+
+Copyright 2012, dbstep Hackers, Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject dbstep "0.1.0-SNAPSHOT"
+  :description "CSV-Data Overtone Dubstep Generator"
+  :url "https://github.com/motin/dbstep"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.0-alpha3"]
+                 [overtone "0.8.0-SNAPSHOT"],
+		 [org.clojure/data.csv "0.1.2"]])
